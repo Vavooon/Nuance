5d70b4d0fa74bf324e768d466a07b6c9073d116f
Vavooon <vavooon@gmail.com>
2015-05-06 20:33:23 +0300

    Add example config

---
+++ app/config/config-example.php
@@ -0,0 +1,17 @@
+<?php
+
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'nuance');
+define('DB_USER', 'nuance');
+define('DB_PASS', 'password');
+define('DB_TABLE_PREFIX', "");
+//define('DB_TABLE_PREFIX', "mesh_");
+// Path should be finished with slash '/'
+
+define('UPLOADED_FILES_PATH',  __DIR__ ."/../../web/ucp/upload/");
+define('CACHE_PATH', __DIR__."/../../cache/");
+
+//define('USE_PHPASS', false);
+
+//define('DEBUG', true);
