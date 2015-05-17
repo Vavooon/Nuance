44ada96f932bdfa0e458ae8a130e437e16d72e3c
Vavooon <vavooon@gmail.com>
2015-05-17 11:49:48 +0300

    Remove concatenated files support. Use HTTP2, Luke

--- web/acp/themes/default/index.php
+++ web/acp/themes/default/index.php
@@ -6,52 +6,44 @@
         <meta content='width=device-width, initial-scale=0.4, maximum-scale=1.0, user-scalable=0' name='viewport' />
         <?php
         $cssPath = realpath(__DIR__);
-        if (file_exists($cssPath . "/gh-buttons.css"))
-        {
-            ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/gh-buttons.css" />
-            <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/reset.css" />
-            <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/style.css" />
-            <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/style-mobile.css" />
-            <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/style-desktop.css" />
-            <?php
-        }
-        else
-        {
-            ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/style.css" />
-            <?php
-        }
         ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/gh-buttons.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/reset.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/style.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/style-mobile.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $usertheme ?>/css/style-desktop.css" />
         <link rel="gettext" type="application/x-po" href="loadplugin.php?target=locale&name=<?php echo configgetvalue('system', 'main', NULL, 'acpLocale') ?>&type=po" />
         <script>
             <?php
-            echo 'var pluginsLoaders = [];';
-            echo 'var debug = ' . (defined('DEBUG') ? 'true' : 'false') . ';';
-            echo 'var userId = ' . ((isset($sessionId) && $sessionId) ? $sessionId : '0') . ';';
-            echo 'var userName = \'' . ((isset($sessionId) && $sessionId) ? $sessionName : '') . '\';';
+            echo "\nvar pluginsLoaders = [];\n";
+            echo "var debug = " . (defined('DEBUG') ? 'true' : 'false') . ";\n";
+            echo "var userId = " . ((isset($sessionId) && $sessionId) ? $sessionId : '0') . ";\n";
+            echo "var userName = '" . ((isset($sessionId) && $sessionId) ? $sessionName : '') . "';\n";
             ?>
         </script>
-            <?php
-            $jsPath = realpath(__DIR__ . "/../../js");
-            if (file_exists($jsPath . "/lib/Gettext.js"))
-            {
-                echo '<script src="js/lib/Gettext.js"></script>';
-                echo '<script src="js/lib/Nuance.class.js"></script>';
-                echo '<script src="js/lib/date.js"></script>';
-            }
-            
-            if (isset($_SESSION['user_id']))
-            {
-                echo '<script src="loadplugin.php?target=plugin&type=js"></script>';
-                echo '<link rel="stylesheet" type="text/css" href="loadplugin.php?target=plugin&type=css" />';
-                echo '<script src="js/admin.js"></script>';
-            }
-            else
-            {
-                echo '<script src="js/auth.js"></script>';
-            }
-            ?>
+        <?php
+        $jsPath = realpath(__DIR__ . "/../../js");
+        ?>
+        <script src="js/lib/Gettext.js"></script>
+        <script src="js/lib/Nuance.class.js"></script>
+        <script src="js/lib/date.js"></script>
+        <?php
+        if (isset($_SESSION['user_id']))
+        {
+        ?>
+        <script src="loadplugin.php?target=plugin&type=js"></script>
+        <link rel="stylesheet" type="text/css" href="loadplugin.php?target=plugin&type=css" />
+        <script src="js/admin.js"></script>
+        <?php
+        }
+        else
+        {
+        ?>
+            <script src="js/auth.js"></script>
+        <?php
+        }
+        ?>
+
     </head>
     <body>
     </body>
